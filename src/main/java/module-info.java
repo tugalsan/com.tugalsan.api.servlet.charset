c57ba9bd13b99db7445db08d7446cba90648e73a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-05 12:58:09 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,5 @@ module com.tugalsan.api.servlet.charset {
     requires com.tugalsan.api.file;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.api.unsafe;
     exports com.tugalsan.api.servlet.charset.server;
 }
